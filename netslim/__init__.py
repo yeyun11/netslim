e46a62ab6229c3e68d13b9a6de9b339369950b8f
yey <33614783+yeyun11@users.noreply.github.com>
2020-09-15 20:06:59 -0700

    Update __init__.py

--- netslim/__init__.py
+++ netslim/__init__.py
@@ -1,7 +1,6 @@
 from .prune import prune, load_pruned_model, network_slimming, uniform_scaling, MaskedBatchNorm, network_slimming_keep_half
 from .sparse import update_bn, update_bn_by_names
 from .graph_parser import get_norm_layer_names, get_pruning_layers
-from .thresholding import css_thresholding
 
 __all__ = [
     "prune",
